3cc2e91464f75e5b7b7faba82a144ef510331ccc
arnav-gudibande <Arnav_Gudibande@Arnav-Gudibandes-MacBook-Pro.local>
2016-02-05 18:27:52 -0800

    update - get/create accounts
    The functions to create and get accounts have been created. Uses the
    argv function from yargs to get user input. Also uses node-persist to
    store data locally to one’s computer.

--- app.js
+++ app.js
@@ -2,6 +2,44 @@ console.log("starting password manager...");
 var storage = require("node-persist");
 storage.initSync();
 
+var argv = require("yargs")
+  .command("create", "Create a New Account", function (yargs) {
+    yargs.options({
+      name: {
+        demand: true,
+        alias: "n",
+        description: "Account Name (eg: Twitter, Facebook)",
+        type: "string"
+      },
+      username: {
+        demand: true,
+        alias: "u",
+        description: "Account Username or Email",
+        type: "string"
+      },
+      password: {
+        demand: true,
+        alias: "p",
+        description: "Account password",
+        type: "string"
+      }
+    }).help('help');
+  })
+  .command("get", "Get an existing account", function (yargs) {
+    yargs.options({
+      name: {
+        demand: true,
+        alias: "n",
+        description: "Account Name (eg: Twitter, Facebook)",
+        type: "string"
+      }
+    }).help("help");
+  })
+  .help("help")
+  .argv;
+
+var command = argv._[0];
+
 function createAccount (account) {
   var accounts = storage.getItemSync("accounts");
   if(typeof accounts === "undefined") {
@@ -9,6 +47,7 @@ function createAccount (account) {
   }
   accounts.push(account);
   storage.setItemSync("accounts", accounts);
+
   return account;
 }
 
@@ -21,14 +60,24 @@ function getAccount (accountName) {
       matchedAccount = account;
     }
   });
+
   return matchedAccount;
 }
 
-//createAccount({
-//  name: "Gmail",
-//  username: "arnav.gudibande@gmail.com",
-//  password: "1234"
-//})
-
-var gmailAccount = getAccount("Gmail");
-console.log(gmailAccount);
+if (command === "create"){
+  var createdAccount = createAccount({
+    name: argv.name,
+    username: argv.username,
+    password: argv.password
+  });
+  console.log("Account Created...");
+  console.log(createdAccount);
+} else if (command === "get"){
+  var fetchedAccount = getAccount(argv.name);
+  if (typeof fetchedAccount === "undefined"){
+    console.log("Account not Found");
+  } else {
+    console.log("Account Found!");
+    console.log(fetchedAccount);
+  }
+}
--- example-args.js
+++ example-args.js
@@ -1,5 +1,27 @@
-var argv = require("yargs").argv;
+var argv = require("yargs")
+  .command("hello", "Greets the user", funtion (yargs) {
+    yargs.options({
+      name: {
+        demand: true,
+        alias: "n",
+        description: "Your first name goes here",
+        type: "String"
+      },
+      lastname: {
+        demand: true;
+        alais: "l",
+        description: "last name goes here",
+        type: "String"
+      }
+    });
+  })
+  .command("get", "getting the account", function (yargs){
+
+  })
+  .help("help")
+  .argv;
 var command = argv._[0];
+
 
 console.log(argv);
 
@@ -7,7 +29,6 @@ if (command === "hello" && typeof argv.name != "undefined" && typeof argv.lastna
   console.log("Hello " + argv.name + " " + argv.lastname + "!");
 } else if (command === "hello" && typeof argv.name != "undefined") {
   console.log("Hello "+ argv.name+"!");
-
 } else if (command === "hello"){
   console.log('Hello world');
 }
